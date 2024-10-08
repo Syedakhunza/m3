72cba032a862a56af60fee8fe1a52daff4d965fc
Syedakhunza <slnu3@my.nl.edu>
2024-10-08 16:08:12 -0500

    Add files via upload

--- p2.py
+++ p2.py
@@ -3,10 +3,11 @@
 
 name = input ("Hello, what's your name?")
 print("hello, " + name + " glad to meet you"+ "!")
-
+#This program helps to greet.
 authorized_names = ("fatima", "ben")
 name = input("what is your name? ")
 if name in authorized_names:
     print("hello, " + name + " nice to meet you!")
 else:
-    print("unauthorized user.")
+    print("unauthorized user")
+    #this program allows only specific users.

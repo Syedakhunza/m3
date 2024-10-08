8200b723ed34bce3605c7349a28828bdcf1b7e96
Syedakhunza <slnu3@my.nl.edu>
2024-10-08 16:03:24 -0500

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,5 @@
+#syeda khunza fatima
+#oct 8,2024
+
+print ("hellooooo worlddd!")
+#simple hello world program
---
+++ p2.py
@@ -0,0 +1,12 @@
+#syeda khunza fatima
+#oct 8,2024
+
+name = input ("Hello, what's your name?")
+print("hello, " + name + " glad to meet you"+ "!")
+
+authorized_names = ("fatima", "ben")
+name = input("what is your name? ")
+if name in authorized_names:
+    print("hello, " + name + " nice to meet you!")
+else:
+    print("unauthorized user.")

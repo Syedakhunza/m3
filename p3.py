fb849d0b40068a7262537b6d828347b813a6f318
Syedakhunza <slnu3@my.nl.edu>
2024-10-10 22:03:45 -0500

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,4 @@
+radius=float(input("enter radius value"))
+area=round(3.14*radius*radius)
+print(f"thanks for using this to calculate area of circle and here is the answer : ",area)
+#This program helps to compute the area of circle.
---
+++ p4.py
@@ -0,0 +1,5 @@
+miles=float(input("enter number of miles car had driven : "))
+gallon=float(input("enter how many gallons of gas is used : "))
+MPG=miles/gallon
+print(f" here is the total MPG of your car : {MPG}")
+#this program helps to compute MPG.
